46c76afdcb1a03c3c99443a2790902ef2dc7a6f4
AndriySt92 <andriyua-1992@mail.ru>
2025-03-28 20:11:04 +0200

    fix: delete authenticate middleware for endpoint /:id/booked-dates in hotel.route.ts

--- routes/hotel.route.ts
+++ routes/hotel.route.ts
@@ -7,7 +7,7 @@ import {
   searchHotels,
   createPayment,
   createBooking,
-  getBookedDates
+  getBookedDates,
 } from '../controllers/hotel.controller'
 import { authenticate } from '../middlewares'
 
@@ -17,7 +17,7 @@ router.get('/search', ctrlWrapper(searchHotels))
 router.get('/countries-summary', ctrlWrapper(getHotelsCountriesSummery))
 router.post('/:id/bookings/payment-intent', authenticate as any, ctrlWrapper(createPayment))
 router.post('/:id/bookings', authenticate as any, ctrlWrapper(createBooking))
-router.get('/:id/booked-dates', authenticate as any, ctrlWrapper(getBookedDates))
+router.get('/:id/booked-dates', ctrlWrapper(getBookedDates))
 router.get('/:id', ctrlWrapper(getHotelById))
 router.get('/', ctrlWrapper(getHotels))
 
